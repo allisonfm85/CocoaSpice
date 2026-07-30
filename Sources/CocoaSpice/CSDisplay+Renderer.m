ef9d618d4161c7f7507c3678c11ee6e48fdeec55
Allison Meloy <allisonfm@gmail.com>
2026-07-29 23:34:10 -0400

    debug: ARC requires __bridge for object-to-void* casts in evidence lines

--- Sources/CocoaSpice/CSDisplay+Renderer.m
+++ Sources/CocoaSpice/CSDisplay+Renderer.m
@@ -31,7 +31,7 @@
 - (void)addRenderer:(id<CSRenderer>)renderer {
     NSArray<id<CSRenderer>> *renderers = self.renderers;
     NSLog(@"[AVM-EV] addRenderer display=%p hadDevice=%d priorRenderers=%lu",
-          (void *)self, self.device != nil, (unsigned long)renderers.count);
+          (__bridge void *)self, self.device != nil, (unsigned long)renderers.count);
     if (![renderers containsObject:renderer]) {
         self.renderers = [renderers arrayByAddingObject:renderer];
     }
